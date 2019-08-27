c357f5f7f9fb433b205eae7b35e9fc67354ed72b
Karla Gallegos <kgalleg618@gmail.com>
2019-08-26 20:55:49 -0500

    sets practice

---
+++ cars.py
@@ -0,0 +1,2 @@
+# Python Sets -A set is a collection of things, like a list, but the collection is both unordered, and contains no duplicate elements. Developers use sets to easily filter down other collections to unique elements, and to see if two, or more, collections share any similar items.
+
